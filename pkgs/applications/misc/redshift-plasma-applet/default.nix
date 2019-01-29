95a8194e42cd5bdd60ba69fed98184b3d2f3c5e2
Unknown <zraexy@gmail.com>
2019-01-28 15:54:28 -0900

    redshift-plasma-applet: cleanup and redshift stopping fixes

--- pkgs/applications/misc/redshift-plasma-applet/default.nix
+++ pkgs/applications/misc/redshift-plasma-applet/default.nix
@@ -1,23 +1,27 @@
-{ stdenv, cmake, extra-cmake-modules, plasma-framework, kwindowsystem, redshift, fetchFromGitHub, }:
+{ stdenv, cmake, extra-cmake-modules, plasma-framework, kwindowsystem, redshift, fetchFromGitHub }:
 
-let version = "1.0.18"; in
-
-stdenv.mkDerivation {
-  name = "redshift-plasma-applet-${version}";
+stdenv.mkDerivation rec {
+  pname = "redshift-plasma-applet";
+  version = "1.0.18";
+  name = "${pname}-${version}";
 
   src = fetchFromGitHub {
     owner = "kotelnik";
     repo = "plasma-applet-redshift-control";
     rev = "v${version}";
     sha256 = "122nnbafa596rxdxlfshxk45lzch8c9342bzj7kzrsjkjg0xr9pq";
-  };
+  }; # Future releases will be fetched from KDE sources
 
   patchPhase = ''
     substituteInPlace package/contents/ui/main.qml \
       --replace "redshiftCommand: 'redshift'" \
                 "redshiftCommand: '${redshift}/bin/redshift'" \
       --replace "redshiftOneTimeCommand: 'redshift -O " \
-                "redshiftOneTimeCommand: '${redshift}/bin/redshift -O "
+                "redshiftOneTimeCommand: '${redshift}/bin/redshift -O " \
+      --replace "redshift -x" \
+                "${redshift}/bin/redshift -x" \
+      --replace "killall redshift" \
+                "killall .redshift-wrapp"
 
     substituteInPlace package/contents/ui/config/ConfigAdvanced.qml \
       --replace "'redshift -V'" \
@@ -36,7 +40,7 @@ stdenv.mkDerivation {
 
   meta = with stdenv.lib; {
     description = "KDE Plasma 5 widget for controlling Redshift";
-    homepage = https://github.com/kotelnik/plasma-applet-redshift-control;
+    homepage = https://phabricator.kde.org/source/plasma-redshift-control;
     license = licenses.gpl2Plus;
     platforms = platforms.linux;
     maintainers = with maintainers; [ benley zraexy ];
